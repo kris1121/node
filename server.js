e42d7158de502a038d884b1e83fd38241ac94ca9
Krzysztof Matyga <krzysztof.matyga@noinputsignal.com>
2023-04-10 23:24:29 +0200

    cors and routes

--- server.js
+++ server.js
@@ -1,40 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const cors = require('cors');
 
-const logEvents = require('./middleware/logEvents');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler'); 
 
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
 
-app.use((req, res, next) => {
-    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'req.Log.txt');
-    console.log(`${req.method} ${req.path}`);
-    next();
-})
+app.use(logger)
+
+//Cross Origin Resource Sharing
+const whitelist = ['http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/subdir', express.static(path.join(__dirname, 'public')));
 
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname})
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
-
-app.get('/new-page(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname})
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
-
-app.get('/old-page(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname})
-    res.redirect(301,'/new-page.html')//302 by default
-})
 
 //route handler
 
@@ -45,26 +47,21 @@ app.get('/hello(.html)?', (req, res, next) => {
     res.send('hello world')
 })
 
-const one = (req, res, next) => {
-    console.log('one');
-    next()
-}
 
-const two = (req, res, next) => {
-    console.log('two');
-    next()
-}
-
-const three = (req, res, next) => {
-    console.log('three');
-    setTimeout(() =>  res.send('Finished'), 2000);
-}
-
-app.get('/chain(.html)?', [one, two, three]);
-
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }
+    else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found"})
+    }
+    else {
+        res.type("txt").send("404 Not Found")
+    }
 })
+
+app.use(errorHandler);    
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
